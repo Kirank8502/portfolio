6381b74f0343c9266a632a1ab8aa6807131298cc
Kiran <kirankidecha85@gmail.com>
2025-12-25 15:57:40 +0000

    environmental file changes

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -18,7 +18,7 @@ const Contact = () => {
     e.preventDefault();
     setIsSubmitting(true);
 
-    const res = await fetch("http://localhost:5000/api/contact", {
+    const res = await fetch("https://portfolio-backend-3bvh.onrender.com/api/contact", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
